6987c4bb2dd5481ebc596b60bee7cb321bf581fc
ALINE <ALINE@DESKTOP-9864R87>
2017-12-21 17:56:08 +0600

    new commit

---
+++ Loop/src/loop/Loop.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loop;
+
+/**
+ *
+ * @author ALINE
+ */
+public class Loop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i = 1;
+        while(i<=100){
+            System.out.println(i);
+            i=i+1;
+            
+            
+        }
+    }
+    
+}
